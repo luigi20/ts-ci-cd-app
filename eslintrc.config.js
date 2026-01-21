e03e09e6312220e1ba3be01352caae072c5f5fde
Luis <luisopentec@gmail.com>
2026-01-21 20:10:27 -0300

    chore: migrate eslint to flat config (v9)

---
+++ eslintrc.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
